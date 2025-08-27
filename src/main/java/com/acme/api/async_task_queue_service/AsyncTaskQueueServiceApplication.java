c3c92b2b6f92980e0af58a17560004244d32cf87
Hector Fontanez <cs.professor.fontanez@gmail.com>
2025-08-26 20:14:11 -0500

    Initial commit - Async Task Queue with Retries and Compensation

---
+++ src/main/java/com/acme/api/async_task_queue_service/AsyncTaskQueueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.acme.api.async_task_queue_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncTaskQueueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncTaskQueueServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/api/async_task_queue_service/AsyncTaskQueueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.api.async_task_queue_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncTaskQueueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
